a950eee59c0ecc6a10b175263ab91374a9e51567
Vishva-01 <vishva01mail@gmail.com>
2024-07-02 09:56:05 +0530

    change

--- RegisterForm/views/__init__.py
+++ RegisterForm/views/__init__.py
@@ -1,1 +1,2 @@
 from .register import registration_form, student_register
+from .student import student_list, mark_as_paid
---
+++ RegisterForm/views/student.py
@@ -0,0 +1,38 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
+from RegisterForm.models import Students
+
+def student_list(request):
+    query = request.GET.get('q', '')
+    is_paid = request.GET.get('is_paid', '')
+    
+    students = Students.objects.all().order_by('-enrolled_on')
+
+    if query:
+        students = students.filter(
+            Q(name__icontains=query) | 
+            Q(email__icontains=query) | 
+            Q(phone_number__icontains=query)
+        )
+    
+    if is_paid:
+        students = students.filter(is_paid=is_paid == 'True')
+
+    paginator = Paginator(students, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'admin/student_list.html', {
+        'page_obj': page_obj,
+        'query': query,
+        'is_paid': is_paid,
+    })
+
+
+def mark_as_paid(request, id):
+    student = get_object_or_404(Students, id=id)
+    student.is_paid = True
+    student.save()
+    return redirect('student_list')
